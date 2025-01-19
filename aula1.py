bd64b72fde16ffb23579e93c20e9f806bbef424e
hugogatti <hugo.gatti@hotmail.com>
2025-01-18 21:21:54 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,10 @@
+# Para comentários iniciar com hashtag
+print(123) # Na frente
+# Abaixo
+
+"""
+DocString
+Não é utilizado como comentário as ASPAS
+"""
+
+''' Usar para escrever notas'''
---
+++ aula2.py
@@ -0,0 +1,4 @@
+# \r \n -> CRLF para quebrar a linha
+print (12, 34, 1011, sep='|')
+print (56, 78, 5025, sep=",")
+print (5, 13, 7855, sep=' ')
