8c6504ab6663c7fbfa75a5bd433d3426c4f7ab29
hugogatti <hugo.gatti@hotmail.com>
2025-01-18 21:26:23 -0300

    Aula2 finalizada função print

--- aula2.py
+++ aula2.py
@@ -1,4 +1,4 @@
 # \r \n -> CRLF para quebrar a linha
-print (12, 34, 1011, sep='|')
-print (56, 78, 5025, sep=",")
-print (5, 13, 7855, sep=' ')
+print (12, 34, 1011, sep='|', end='\n##\n')
+print (56, 78, 5025, sep=",", end='\n')
+print (5, 13, 7855, sep=' ', end='\n')
